b37d2134e5d9bbd4066cb36903d8e76d08c9ca45
redlikedeath <charles.bonn@mines.sdsmt.edu>
2016-03-23 20:04:04 -0600

    working random games for code

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ window.location=url;
 }
 function rand_game() {
 var a;
-a = 1+Math.round(Math.random()*3);   // a = random number between 1-3
+a = Math.floor((Math.random()*3) +1 );   // a = random number between 1-3
 if (a==1) return("games/dots/Dots II/dots-ii.swf", game_name = "games/dots/Dots II/dots-ii.swf" );
 if (a==2) return("games/assembots/Assembots/assembots.swf", game_name = "games/assembots/Assembots/assembots.swf");
 if (a==3) return("games/bedazzled/Bedazzled/bedazzled.swf", game_name = "games/bedazzled/Bedazzled/bedazzled.swf");
